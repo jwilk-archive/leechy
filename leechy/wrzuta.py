a104cf3154d0f40e5db504949333d176051dfd10
Jakub Wilk <jwilk@jwilk.net>
2009-09-09 23:19:30 +0200

    wrzuta: initial support for http://*.wrzuta.pl/.

---
+++ leechy/wrzuta.py
@@ -0,0 +1,37 @@
+# encoding=UTF-8
+
+# Copyright © 2009 Jakub Wilk <ubanus@users.sf.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+from leechy import Browser
+
+class Browser(Browser):
+
+    pattern = r'^http://\w+[.]wrzuta[.]pl/(film|audio)/\w+/[^/]+$'
+
+    def download(self):
+
+        import urlparse
+        import os
+        protocol, host, target, _, _, _ = urlparse.urlparse(self.start_uri)
+        _, type, id, target = target.split('/')
+        if type == 'film':
+            target += '.flv'
+            uri = '/sr/f/%(id)s/0/' % locals()
+        elif type == 'audio':
+            target += '.mp3'
+            uri = '/sr/f/%(id)s' % locals()
+        else:
+            raise RuntimeError
+            self.report_api_error('filetype')
+        uri = urlparse.urlunparse((protocol, host, uri, None, None, None))
+        self.wget(uri, target)
+
